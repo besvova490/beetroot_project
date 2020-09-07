e6879ecaefff4f1557a944f10114022ad9d4f6fc
besvova490 <besvova490@gmail.com>
2020-09-07 14:34:40 +0300

    api update

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -10,5 +11,8 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
+
+CORS(app, supports_credentials=True)
+
 
 from app import routes, models
